e6bbd4a9b139b19c35b88e890cd78bd619a61265
yoshi380 <ai.tourmaline@gmail.com>
2025-06-20 12:11:48 +0900

    Delete modules directory

--- modules/getFishingIndex.js
+++
@@ -1,8 +0,0 @@
-function getFishingIndex(windSpeed) {
-  if (windSpeed <= 2) return '◎';
-  if (windSpeed <= 4) return '◯';
-  if (windSpeed <= 6) return '△';
-  return '×';
-}
-
-module.exports = getFishingIndex;
